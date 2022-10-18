9bca4846d3a468f0a17635546ef7691301b111b0
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-18 12:39:33 -0500

    reorg

--- swiftui-mapkit/ViewModels/ViewModel.swift
+++ swiftui-mapkit/ViewModels/ViewModel.swift
@@ -1,6 +1,6 @@
 import MapKit
 
-class ViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+class ViewModel: NSObject, ObservableObject {
     @Published var annotations: [Place] = []
     @Published var region = MKCoordinateRegion()
 
@@ -18,68 +18,23 @@ class ViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
 
     override init() {
         super.init()
+
         manager.delegate = self
+
         // Won't find current location without this.
         manager.desiredAccuracy = 25 // in meters
+
         // It fails to find the current location when this is 20 or below!
 
-        let center = CLLocationCoordinate2D(
-            latitude: 51.501,
-            longitude: -0.1425
-        )
-        region = MKCoordinateRegion(
-            center: center,
-            latitudinalMeters: size,
-            longitudinalMeters: size
-        )
-
         Task {
-            for place in initialPlaces {
-                await search(place)
-            }
+            await setup()
         }
     }
 
+    @MainActor
     func clearAnnotations() {
         annotations = []
         if let place = selectedPlace { annotations.append(place) }
-    }
-
-    func locationManager(
-        _: CLLocationManager,
-        didUpdateLocations locations: [CLLocation]
-    ) {
-        print("locations.count = \(locations.count)")
-        if let coordinates = locations.first?.coordinate {
-            let location = CLLocation(
-                latitude: coordinates.latitude,
-                longitude: coordinates.longitude
-            )
-            // Guess the address of the current location.
-            CLGeocoder().reverseGeocodeLocation(location) { places, _ in
-                if let place = places?.first {
-                    let placemark = MKPlacemark(placemark: place)
-                    let item = MKMapItem(placemark: placemark)
-                    item.name = "You Are Here"
-                    self.selectedPlace = Place(item: item, location: coordinates)
-                } else {
-                    self.selectedPlace = Place(name: "You Are Here", location: coordinates)
-                }
-                self.annotations.append(self.selectedPlace!)
-            }
-        }
-    }
-
-    func locationManager(
-        _: CLLocationManager,
-        didFailWithError _: Error
-    ) {
-        print("failed to get current location - user may not have approved")
-        // The Simulator cannot get the current location.
-        // After the user denies sharing location, they can
-        // open their Settings app,  go to Privacy ... Location Services,
-        // tap the name of this app, and select
-        // Never, Ask Next Time, or While Using the App.
     }
 
     func search(_ searchText: String) async {
@@ -103,5 +58,71 @@ class ViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
         } else {
             print("no results found")
         }
+    }
+
+    @MainActor
+    private func setup() async {
+        let center = CLLocationCoordinate2D(
+            latitude: 51.501,
+            longitude: -0.1425
+        )
+        region = MKCoordinateRegion(
+            center: center,
+            latitudinalMeters: size,
+            longitudinalMeters: size
+        )
+
+        for place in initialPlaces {
+            await search(place)
+        }
+    }
+}
+
+extension ViewModel: CLLocationManagerDelegate {
+    func locationManager(
+        _: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        if let coordinates = locations.first?.coordinate {
+            let location = CLLocation(
+                latitude: coordinates.latitude,
+                longitude: coordinates.longitude
+            )
+            // Guess the address of the current location.
+            CLGeocoder().reverseGeocodeLocation(location) { places, _ in
+                if let place = places?.first {
+                    let placemark = MKPlacemark(placemark: place)
+                    let item = MKMapItem(placemark: placemark)
+                    item.name = "You Are Here"
+                    self.selectedPlace = Place(
+                        item: item,
+                        location: coordinates
+                    )
+                } else {
+                    self.selectedPlace = Place(
+                        name: "You Are Here",
+                        location: coordinates
+                    )
+                }
+
+                Task {
+                    await MainActor.run {
+                        self.annotations.append(self.selectedPlace!)
+                    }
+                }
+            }
+        }
+    }
+
+    func locationManager(
+        _: CLLocationManager,
+        didFailWithError _: Error
+    ) {
+        print("failed to get current location - user may not have approved")
+        // The Simulator cannot get the current location.
+        // After the user denies sharing location, they can
+        // open their Settings app,  go to Privacy ... Location Services,
+        // tap the name of this app, and select
+        // Never, Ask Next Time, or While Using the App.
     }
 }
