a467b9665c99da36b2fdca67ab3f997cd8184a29
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-19 13:37:54 -0500

    added comments

--- swiftui-mapkit/ViewModels/MapSettings.swift
+++ swiftui-mapkit/ViewModels/MapSettings.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 final class MapSettings: ObservableObject {
-    @Published var elevation = "realistic"
-    @Published var emphasis = "default"
-    @Published var type = "hybrid"
+    @Published var elevation = "realistic" // other is "flat"
+    @Published var emphasis = "default" // other is "muted"
+    @Published var type = "hybrid" // other are "standard" and "image"
 }
