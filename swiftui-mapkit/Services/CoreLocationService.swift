0dd78a70b80fad7aeca40b637a33b54e886ca0be
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-25 15:00:58 -0500

    progress

--- swiftui-mapkit/Services/CoreLocationService.swift
+++ swiftui-mapkit/Services/CoreLocationService.swift
@@ -4,18 +4,17 @@ import SwiftUI
 struct CoreLocationService {
     static func getPlacemark(from location: CLLocation) async throws
         -> CLPlacemark? {
-        try await withCheckedThrowingContinuation { continuation in
-            // Cannot call this more than 50 times per second.
-            CLGeocoder().reverseGeocodeLocation(location) { placemark, error in
-                if let error {
-                    continuation.resume(throwing: error)
-                } else {
-                    continuation.resume(returning: placemark?.first)
-                }
-            }
+        // Cannot call this more than 50 times per second.
+        let placemarks = try await CLGeocoder()
+            .reverseGeocodeLocation(location)
+        if let placemark = placemarks.first {
+            return placemark
+        } else {
+            throw "no placemarks found"
         }
     }
 
+    // This is not currently used.
     static func getPlacemark(from addressString: String) async throws
         -> CLPlacemark {
         // Cannot call this more than 50 times per second.
