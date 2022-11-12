4ff9e87b0b4b9226482774bdfc14becd1c22f91d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-11-12 11:05:05 -0600

    added dump function for debugging

--- swiftui-mapkit/utilities.swift
+++ swiftui-mapkit/utilities.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+func dump(title: String, object: AnyObject) {
+    print(title)
+    let mirror = Mirror(reflecting: object)
+    for property in mirror.children {
+        let label = String(describing: property.label)
+        print("\(label) = \(property.value)")
+    }
+}
+
 func mainQ(closure: @escaping () -> Void) {
     Task<Sendable, Error> {
         await MainActor.run {
